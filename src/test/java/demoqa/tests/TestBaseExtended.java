5b3c9a46f09d0d9ec792d89b4e548f47969d00d4
elenachaplygina <antonenko-elena@inbox.ru>
2023-02-12 23:24:15 +0300

    added attachs - screenshot,logs

--- src/test/java/demoqa/tests/TestBaseExtended.java
+++ src/test/java/demoqa/tests/TestBaseExtended.java
@@ -2,7 +2,9 @@ package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import demoqa.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,11 +16,11 @@ public class TestBaseExtended {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.browser="chrome";
-        Configuration.browserVersion="100.0";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
         Configuration.baseUrl = "https://demoqa.com";
 //        Configuration.holdBrowserOpen = true;
-        Configuration.remote="https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
@@ -29,12 +31,20 @@ public class TestBaseExtended {
 //                URI.create("http://selenoid:4444/wd/hub").toURL(),
 //                capabilities
 //        );
-        Configuration.browserCapabilities=capabilities;
+        Configuration.browserCapabilities = capabilities;
     }
 
 
     @BeforeEach
-    void addListener(){
-        SelenideLogger.addListener("AllureSelenide",new AllureSelenide());
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+//        Attach.video();
     }
 }
